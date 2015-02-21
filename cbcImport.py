e371f7b55fa7de04d0e043007cab183d96455935
ch4noyu <ch4noyu@yahoo.com>
2015-02-21 14:40:22 +0100

    add: saving rest, added

--- cbcImport.py
+++ cbcImport.py
@@ -17,8 +17,21 @@ class cbcImport():
 		""" init """
 		# file to import
 		self.importFile=os.path.expanduser("~/Desktop/tangorin_38567.csv")
+		self.delim='\t'
+		# character encoding to be used for in- and outputu
+		self.encoding='utf-8'
+		importFileName,importFileExt=os.path.splitext(self.importFile)
+		# file where the subset of added/remaining Cards will be Saved
+		# (by default: self.importFile - file ending + "added"/"rest" + extension)
+		# Note that the file contents will be overwritten!
+		# If self.addedFile=None or self.addedFile="" is specified
+		# no output will be created for addedFile (restFile analogous)
+		self.addedFile=importFileName+"_added"+importFileExt
+		self.restFile=importFileName+"_rest"+importFileExt
 		# data from import File will be saved in self.data:
 		self.data=[] # format: [(field1,field2,...)]
+		self.added=[]
+		self.rest=[]
 		# how many of the vocabulary have we already added:
 		self.currentIdx=0
 		# saves the instance of the Editor window
@@ -28,9 +41,9 @@ class cbcImport():
 		""" Loads self.importFile to self.data. """
 		self.data=[]
 		with open(self.importFile,'r') as csvfile:
-			reader=csv.reader(csvfile, delimiter="\t")
+			reader=csv.reader(csvfile, delimiter=self.delim)
 			for row in reader:
-				self.data.append(row)
+				self.data.append([c.decode(self.encoding) for c in row])
 		self.updateStatus()
 	def clean(self):
 		""" Resets all fields. """
@@ -64,8 +77,9 @@ class cbcImport():
 		current=self.data[self.currentIdx]
 		# ADAPT this!
 		# ---------------
-		self.e.note['Expression']=unicode(current[0].split('・')[-1],'utf-8')
-		self.e.note['Meaning']=unicode(current[2],'utf-8')
+		delim=unicode('・',self.encoding)
+		self.e.note['Expression']=current[0].split(delim)[-1]
+		self.e.note['Meaning']=current[2]
 		# ---------------
 		self.e.note.flush()
 		self.e.loadNote()
@@ -82,14 +96,31 @@ class cbcImport():
 			fieldIdx=mw.col.models.fieldNames(self.e.note.model()).index('Expression')
 			runHook('editFocusLost',False,self.e.note,fieldIdx)
 		self.e.loadNote()
-	def history(self,obj, note):
-		print(note)
-		print(note['Expression'])
-	
-	def cardAdded(self,AddCardsObj):
-		note=AddCardsObj.editor.note
-		print(note)
-		print(note['Expression'])
+	def cardAdded(self, obj, note):
+		current=self.data[self.currentIdx]
+		print(note['Expression'],current[0])
+		if note['Expression'] in current[0]:
+			print("added")
+			self.added.append(current)
+		self.updateStatus()
+	def updateRest(self):
+		for i in range(len(self.data)):
+			if not self.data[i] in self.added:
+				self.rest.append(self.data[i])
+	def save(self):
+		print("Saving")
+		with open(self.addedFile,'wb') as csvfile:
+			writer=csv.writer(csvfile, delimiter=self.delim)
+			for row in self.added:
+				row=[c.encode(self.encoding) for c in row]
+				writer.writerow(row)
+		self.updateRest()
+		with open(self.restFile,'wb') as csvfile:
+			writer=csv.writer(csvfile, delimiter=self.delim)
+			for row in self.rest:
+				row=[c.encode(self.encoding) for c in row]
+				writer.writerow(row)
+		
 	def wrapper(self,AddCardsObj):
 		self.mySetupButtons(AddCardsObj.editor)
 	
@@ -105,14 +136,15 @@ class cbcImport():
 			self.newIconsBox.setSpacing(14)
 		self.e.outerLayout.addLayout(self.newIconsBox)
 		# --
-		self.myAddButton("cbcLoad", self.load, text="Load", tip="Load file", size=False, )
-		self.myAddButton("cbcReverse", self.reverse, text="Reverse", tip="Reverse Order", size=False, )
-		self.myAddButton("cbcFirst", self.first, text="<<", tip="Fill in first entry", size=False,)
-		self.myAddButton("cbcPrevious", self.previous, text="<", tip="Fill in previous entry (Ctrl+H)", size=False , key="Ctrl+H")
-		self.myAddButton("cbcFill", self.insert, text="X", tip="Fill in form (Ctrl+F)", size=False,  key="Ctrl+F")
-		self.myAddButton("cbcNext", self.next, text=">", tip="Fill in next entry (Ctrl+G)", size=False, key="Ctrl+G")
-		self.myAddButton("cbcLast", self.last, text=">>", tip="Fill in last entry", size=False , )
+		self.myAddButton("cbcLoad", self.load, text="Load", tip="Load file", size="30x60", )
+		self.myAddButton("cbcReverse", self.reverse, text="Reverse", tip="Reverse Order", size="30x60", )
+		self.myAddButton("cbcFirst", self.first, text="<<", tip="Fill in first entry", size="30x30",)
+		self.myAddButton("cbcPrevious", self.previous, text="<", tip="Fill in previous entry (Ctrl+H)", size="30x30" , key="Ctrl+H")
+		self.myAddButton("cbcFill", self.insert, text="X", tip="Fill in form (Ctrl+F)", size="30x30",  key="Ctrl+F")
+		self.myAddButton("cbcNext", self.next, text=">", tip="Fill in next entry (Ctrl+G)", size="30x30", key="Ctrl+G")
+		self.myAddButton("cbcLast", self.last, text=">>", tip="Fill in last entry", size="30x30" , )
 		self.status=QLabel()
+		self.updateStatus()
 		self.newIconsBox.addWidget(self.status)
 	
 	def reverse(self):
@@ -127,9 +159,10 @@ class cbcImport():
 			b.connect(b, SIGNAL("clicked(bool)"), func)
 		else:
 			b.connect(b, SIGNAL("clicked()"), func)
-		if size:
-			b.setFixedHeight(20)
-			b.setFixedWidth(60)
+#		if size:
+#			if size.split('x')[0]:
+#				b.setFixedHeight(int(size.split('x')[0]))
+#			b.setFixedWidth(int(size.split('x')[1]))
 		if not text:
 			b.setIcon(QIcon(":/icons/%s.png" % name))
 		if key:
@@ -146,8 +179,7 @@ class cbcImport():
 	def updateStatus(self):
 		""" Updates button texts e.g. to display 
 		number of remaining entries etc. """
-		if not len(self.data)==0:
-			self.status.setText("%d/%d" % (self.currentIdx+1,len(self.data)))	
+		self.status.setText(" Idx: %d/%d Add: %d Rem: %d" % (self.currentIdx+1,len(self.data),len(self.added),len(self.data)-len(self.added)))	
 
 def addNoteHook(*args):
 	runHook("cardAdded",*args)
@@ -159,5 +191,6 @@ myImport=cbcImport()
 AddCards.addHistory=wrap(AddCards.addHistory,lambda *args: runHook("addHistory",*args))
 AddCards.setupEditor=wrap(AddCards.setupEditor,lambda AddCardsObj: runHook("addEditorSetup",AddCardsObj))
 
-addHook("addHistory",myImport.history)
+addHook("addHistory",myImport.cardAdded)
 addHook("addEditorSetup",myImport.wrapper)
+addHook("unloadProfile",myImport.save)
